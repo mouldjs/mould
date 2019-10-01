779d0b8a9754814977238cd617e65717e127e65c
miehou <>
2019-10-01 14:35:19 +0800

    commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "next/head";
+
+const Home = () => (
+    <div>
+        <Head>
+            <title>Home</title>
+            <link rel="icon" href="/static/favicon.ico" importance="low" />
+        </Head>
+        index
+    </div>
+);
+
+export default Home;
