80c7e4158e4c388cddac54e1d0293eee4347b95d
kayneluo <kayne.luo@binance.com>
2020-05-06 18:22:47 +0800

    chore(*): add js module alias

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 const withCSS = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
-const { resolve } = require('path')
+const path = require('path')
 
 module.exports = withSass(
     withCSS({
         /* config options here */
         webpack(config, options) {
-            config.resolve.alias['@'] = resolve(__dirname)
+            config.resolve.alias['@'] = path.join(__dirname)
             return config
         },
     })
