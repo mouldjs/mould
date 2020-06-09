21f5e19b7022ed05274a82462924e5c50560083b
Rostislav Bobrovsky <rostislav.bobrovsky@gmail.com>
2020-06-09 20:53:22 +0300

    feat(cli): update dev script to watch mould directory (#55)

--- cli/dev.js
+++ cli/dev.js
@@ -2,6 +2,7 @@
 
 const spawn = require('cross-spawn')
 const fs = require('fs')
+const { debounce } = require('lodash')
 const path = require('path')
 
 const { MOULD_DIRECTORY } = require('./constants')
@@ -14,6 +15,15 @@ if (fs.existsSync(mouldPath)) {
     spawn('bash', ['-c', `cd ${appPath} && WORKDIR=${mouldPath} next dev`], {
         stdio: 'inherit',
     })
+
+    fs.watch(
+        mouldPath,
+        debounce((event, filename) => {
+            if (filename) {
+                console.log(`${filename} file was changed`)
+            }
+        }, 500)
+    )
 } else {
     console.warn(
         `You don't have ${MOULD_DIRECTORY} initialized at ${originalDirectory}\n\n` +
