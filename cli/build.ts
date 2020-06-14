1c8756f787cff8584e540c5694e04542ec7f754b
Rostislav Bobrovsky <rostislav.bobrovsky@gmail.com>
2020-06-14 12:38:32 +0300

    feat(cli): add build script

---
+++ cli/build.ts
@@ -0,0 +1,27 @@
+import fs from 'fs'
+import path from 'path'
+
+import { MOULD_DIRECTORY } from './constants'
+
+const originalDirectory: string = process.cwd()
+
+const mouldPath = path.join(originalDirectory, MOULD_DIRECTORY)
+const schemaPath = path.join(mouldPath, '.mould')
+
+if (fs.existsSync(schemaPath)) {
+    fs.readFile(schemaPath, 'utf8', (err, schema) => {
+        console.log(JSON.parse(schema))
+    })
+} else if (fs.existsSync(mouldPath)) {
+    console.warn(
+        `You don't have Mould Schema at ${mouldPath}\n\n` +
+            'You could begin by typing:\n\n' +
+            '  mould dev\n'
+    )
+} else {
+    console.warn(
+        `You don't have ${MOULD_DIRECTORY} initialized at ${originalDirectory}\n\n` +
+            'You could start by typing:\n\n' +
+            '  mould init\n'
+    )
+}
