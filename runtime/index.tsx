fc36553aa286749f262ae71d9078ad848a9f6ca1
chienweiluo <chienweiluo@gmail.com>
2020-06-18 17:45:06 +0800

    Fix/debug (#83)
    * fix(runtime): text missing when kit wraps root component in Debug
    
    * fix(view): cannot select debugging view and display debug status
    
    * fix(runtime): let content key use value in scope before the textProp
    
    * fix(runtime/index): remove rootCOmponent
    
    Co-authored-by: kayneluo <kayne.luo@binance.com>

--- runtime/index.tsx
+++ runtime/index.tsx
@@ -32,110 +32,105 @@ export const runtime = (moulds: Mould[]) => {
                 pick(Object.keys(input), rest)
             )
 
-            const renderChildren = (components: Component[]) => {
-                return components.map((component) => {
-                    const Plugin = Components.find(
-                        (c) => c.type === component.type
-                    )
+            const renderComp = (component, isRoot) => {
+                const Plugin = Components.find((c) => c.type === component.type)
 
-                    if (!Plugin) {
-                        throw Error(`Can not find plugin: ${component.type}`)
+                if (!Plugin) {
+                    throw Error(`Can not find plugin: ${component.type}`)
+                }
+
+                let Comp = Plugin.Raw
+                let transform = Plugin.Transform
+
+                let { props, children } = component
+                if (component.type === 'Kit') {
+                    const kitName = (component as any).props.__kitName
+                    const kit = kits.find((k) => k.name === kitName)
+                    if (!kit) {
+                        throw Error(`Can not find kit: ${kitName}`)
                     }
+                    const { isList } = kit
+                    if (!isList) {
+                        const kitProps = { ...props }
+                        kit!.dataMappingVector.forEach(
+                            ([propField, scopeField]) => {
+                                kitProps[propField] = scope[scopeField]
+                            }
+                        )
+                        props = kitProps
+                        if (kit?.type === 'Mould') {
+                            props = {
+                                ...props,
+                                __mouldName: (kit!.param as any).mouldName,
+                            } as any
+                            Comp = RuntimeMould
+                        } else {
+                            const Plugin = Components.find(
+                                (c) => c.type === kit.type
+                            )
+                            if (!Plugin) {
+                                throw Error(
+                                    `Can not find plugin in kit: plugin ${kit.type}, kit ${kit.name}`
+                                )
+                            }
 
-                    let Comp = Plugin.Raw
-                    let transform = Plugin.Transform
-
-                    let { props, children } = component
-                    if (component.type === 'Kit') {
-                        const kitName = (component as any).props.__kitName
-                        const kit = kits.find((k) => k.name === kitName)
-                        if (!kit) {
-                            throw Error(`Can not find kit: ${kitName}`)
+                            Comp = Plugin.Raw
+                            transform = Plugin.Transform
                         }
-                        const { isList } = kit
-                        if (!isList) {
-                            const kitProps = { ...props }
+                    } else {
+                        Comp = List
+                        const scopeForThis = scope[kitName]
+                        children = scopeForThis.map((scope) => {
+                            let kitProps = {}
                             kit!.dataMappingVector.forEach(
                                 ([propField, scopeField]) => {
                                     kitProps[propField] = scope[scopeField]
                                 }
                             )
-                            props = kitProps
                             if (kit?.type === 'Mould') {
-                                props = {
-                                    ...props,
-                                    __mouldName: (kit!.param as any).mouldName,
+                                kitProps = {
+                                    ...kitProps,
+                                    __mouldName: (kit.param as any).mouldName,
                                 } as any
-                                Comp = RuntimeMould
-                            } else {
-                                const Plugin = Components.find(
-                                    (c) => c.type === kit.type
-                                )
-                                if (!Plugin) {
-                                    throw Error(
-                                        `Can not find plugin in kit: plugin ${kit.type}, kit ${kit.name}`
-                                    )
-                                }
-
-                                Comp = Plugin.Raw
-                                transform = Plugin.Transform
                             }
-                        } else {
-                            Comp = List
-                            const scopeForThis = scope[kitName]
-                            children = scopeForThis.map((scope) => {
-                                let kitProps = {}
-                                kit!.dataMappingVector.forEach(
-                                    ([propField, scopeField]) => {
-                                        kitProps[propField] = scope[scopeField]
-                                    }
-                                )
-                                if (kit?.type === 'Mould') {
-                                    kitProps = {
-                                        ...kitProps,
-                                        __mouldName: (kit.param as any)
-                                            .mouldName,
-                                    } as any
-                                }
 
-                                return { type: kit?.type, props: kitProps }
-                            })
-                        }
+                            return { type: kit?.type, props: kitProps }
+                        })
                     }
+                }
 
-                    if (component.type === 'Mould') {
-                        Comp = RuntimeMould
-                    }
+                if (component.type === 'Mould') {
+                    Comp = RuntimeMould
+                }
 
-                    if (!Comp) {
-                        return null
-                    }
+                if (!Comp) {
+                    return null
+                }
 
-                    const styledProps = transform ? transform(props) : {}
-                    // const styledProps = {}
+                const styledProps = transform ? transform(props) : {}
+                // const styledProps = {}
 
-                    return (
-                        <Comp {...styledProps} {...props}>
-                            {children && renderChildren(children)}
-                        </Comp>
-                    )
-                })
+                return (
+                    <Comp
+                        {...styledProps}
+                        {...props}
+                        ref={isRoot ? ref : undefined}
+                    >
+                        {children && renderChildren(children)}
+                    </Comp>
+                )
+            }
+
+            const renderChildren = (components: Component[]) => {
+                return components.map((c) => renderComp(c, false))
             }
 
             const rootComponent = states[currentState]
             if (!rootComponent) {
                 return null
             }
-            const RootComp = Components.find(
-                (c) => c.type === rootComponent.type
-            )!.Raw
 
-            return (
-                <RootComp {...rootComponent.props} ref={ref}>
-                    {rootComponent.children &&
-                        renderChildren(rootComponent.children)}
-                </RootComp>
-            )
+            return renderComp(rootComponent, true)
         }
     )
 
