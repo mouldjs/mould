4d8a2b5fbf8965b3ec4bd368743c84593c4f8082
Rostislav Bobrovsky <rostislav.bobrovsky@gmail.com>
2020-06-27 21:50:25 +0300

    feat(cli): add copyByExtensionWithExtensionReplacement function

--- cli/copy.js
+++ cli/copy.js
@@ -1,17 +1,37 @@
 import fs from 'fs'
 import path from 'path'
 
-export async function copyByExtension(
-    directoryPath,
-    destinationPath,
-    extension
+import { replaceExtension } from './utils'
+
+export async function copyFiles(
+    srcPath,
+    destPath,
+    { getDestFilename = (file) => file, filterByFilename = () => true } = {}
 ) {
-    for (const file of await fs.promises.readdir(directoryPath)) {
-        if (path.extname(file) === extension) {
+    for (const file of await fs.promises.readdir(srcPath)) {
+        if (filterByFilename(file)) {
             await fs.promises.copyFile(
-                path.join(directoryPath, file),
-                path.join(destinationPath, file)
+                path.join(srcPath, file),
+                path.join(destPath, getDestFilename(file))
             )
         }
     }
+}
+
+export async function copyByExtension(srcPath, destPath, ext) {
+    await copyFiles(srcPath, destPath, {
+        filterByFilename: (file) => path.extname(file) === ext,
+    })
+}
+
+export async function copyByExtensionWithExtensionReplacement(
+    srcPath,
+    destPath,
+    ext,
+    extReplacement
+) {
+    await copyFiles(srcPath, destPath, {
+        getDestFilename: (file) => replaceExtension(file, extReplacement),
+        filterByFilename: (file) => path.extname(file) === ext,
+    })
 }
