e24930622c18643619237f3ba914a64d77a193b6
lightrabbit <lightpacerabbit@gmail.com>
2020-07-03 16:46:37 +0800

    fix(workspace): fix zoom issue while window resize

--- lib/useClientRect.ts
+++ lib/useClientRect.ts
@@ -1,16 +1,31 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useState, useEffect } from 'react'
 
 function useClientRect(): [
     ClientRect | undefined,
     (node: HTMLDivElement) => void
 ] {
     const [rect, setRect] = useState<ClientRect>()
+    const [node, setNode] = useState<HTMLDivElement>()
 
     const ref = useCallback((node: HTMLDivElement) => {
         if (node !== null) {
-            setRect(node.getBoundingClientRect())
+            setNode(node)
         }
     }, [])
+    useEffect(() => {
+        function resizeListener() {
+            if (node) {
+                setRect(node.getBoundingClientRect())
+            }
+        }
+        if (node) {
+            setRect(node.getBoundingClientRect())
+            window.addEventListener('resize', resizeListener)
+        }
+        return () => {
+            window.removeEventListener('resize', resizeListener)
+        }
+    }, [node])
 
     return [rect, ref]
 }
