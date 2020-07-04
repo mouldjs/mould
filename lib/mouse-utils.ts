8d0daf49f108814212f9abae8f4034c202fcab05
lightrabbit <lightpacerabbit@gmail.com>
2020-07-05 00:56:17 +0800

    fix(workspace): Improve the detection strategy of the zoom event source device (#115)

--- lib/mouse-utils.ts
+++ lib/mouse-utils.ts
@@ -6,28 +6,34 @@ interface ExtendedWheelEvent extends WheelEvent {
     wheelDeltaY?: number
 }
 
-export function wheelEventType(
-    event: ExtendedWheelEvent
-): 'mousewheel' | 'touchpad' {
-    const { deltaX, deltaY } = event
-    if (
-        deltaY * -3 === event.wheelDeltaY ||
-        Math.abs(deltaX) >= Math.abs(deltaY)
-    ) {
-        return 'touchpad'
-    }
-    return 'mousewheel'
-}
+let zoomContext: 'mousewheel-mac' | 'mousewheel-win' | 'touchpad' = 'touchpad'
 
-export function getPinchGestureEventSource(
-    event: FullGestureState<'pinch'>
-): 'mousewheel' | 'touchpad' {
-    event.event?.persist()
-    const nativeEvent = event.event?.nativeEvent
-    if (nativeEvent instanceof WheelEvent) {
-        if (wheelEventType(nativeEvent) === 'mousewheel') {
-            return 'mousewheel'
+export function normalizeZoomFactor(e: FullGestureState<'pinch'>): number {
+    let factor = e.delta[1]
+
+    if (e.first && e.event?.type === 'wheel') {
+        zoomContext = 'touchpad'
+
+        if (Math.abs(Math.abs(factor) - 4) < 0.0003) {
+            // In MacOS Chrome mousewheel first absolute delta always equal 4.00244140625
+            zoomContext = 'mousewheel-mac'
+        }
+
+        const dividedFactor = factor / (1 / 3)
+        if (Math.abs(Math.round(dividedFactor) - dividedFactor) < 0.00004) {
+            // In Windows Chrome all mousewheel deltaY is divisible by 1/3
+            zoomContext = 'mousewheel-win'
         }
     }
-    return 'touchpad'
+
+    const direction = factor > 0 ? 1 : factor < 0 ? -1 : 0
+
+    switch (zoomContext) {
+        case 'mousewheel-mac':
+            return direction * 30
+        case 'mousewheel-win':
+            return direction * 30
+        default:
+            return factor
+    }
 }
