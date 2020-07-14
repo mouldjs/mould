15ebc0f11d80c0cfe37fe6a5226787634bfcad47
lightrabbit <lightpacerabbit@gmail.com>
2020-07-14 23:11:18 +0800

    feat(standard): add zod type based event handler picker add keyboard events

--- standard/common.ts
+++ standard/common.ts
@@ -2,6 +2,38 @@ import * as z from 'zod'
 
 const Initial = z.object({})
 
+function zEventHandler<T extends z.ZodType<any>>(eventType: T) {
+    return z.function(z.tuple([eventType]), z.any())
+}
+
+export function pickEventHandlers<
+    T extends z.ZodObject<
+        {
+            [key in string]: z.ZodUnion<
+                [z.ZodFunction<z.ZodTuple<[any]>, z.ZodAny>, z.ZodUndefined]
+            >
+        }
+    >,
+    U extends z.infer<T>
+>(
+    eventHandelerDefinition: T,
+    props: U,
+    wrapper?: <V>(func: V) => V
+): z.infer<T> {
+    const keys = Object.keys(eventHandelerDefinition.shape)
+    const obj = {}
+    if (wrapper) {
+        keys.forEach((key) => {
+            obj[key] = wrapper(props[key])
+        })
+    } else {
+        keys.forEach((key) => {
+            obj[key] = props[key]
+        })
+    }
+    return obj
+}
+
 export const Shadow = z.object({
     shadow: z.string().optional(),
 })
@@ -38,14 +70,44 @@ export const Layout = z.object({
     flexShrink: z.string().optional(),
 })
 
-export const CommonMouseEvent = z.object({
+export const BaseEvent = z.object({
+    nativeEvent: z.any(),
+    currentTarget: z.any(),
+    target: z.any(),
+    bubbles: z.boolean(),
+    cancelable: z.boolean(),
+    defaultPrevented: z.boolean(),
+    eventPhase: z.number(),
+    isTrusted: z.boolean(),
+    preventDefault: z.function(z.tuple([]), z.undefined()),
+    isDefaultPrevented: z.function(z.tuple([]), z.boolean()),
     stopPropagation: z.function(z.tuple([]), z.undefined()),
+    isPropagationStopped: z.function(z.tuple([]), z.boolean()),
+    persist: z.function(z.tuple([]), z.undefined()),
+    timeStamp: z.number(),
+    type: z.string(),
 })
 
-const DefaultMouseEventHandler = z.function(
-    z.tuple([CommonMouseEvent]),
-    z.undefined()
-)
+export const CommonMouseEvent = BaseEvent.augment({
+    altKey: z.boolean(),
+    button: z.number(),
+    buttons: z.number(),
+    clientX: z.number(),
+    clientY: z.number(),
+    ctrlKey: z.boolean(),
+    metaKey: z.boolean(),
+    movementX: z.number(),
+    movementY: z.number(),
+    pageX: z.number(),
+    pageY: z.number(),
+    relatedTarget: z.any(),
+    screenX: z.number(),
+    screenY: z.number(),
+    shiftKey: z.boolean(),
+    type: z.number(),
+})
+
+const DefaultMouseEventHandler = zEventHandler(CommonMouseEvent)
 
 const OptionalDefaultMouseEventHandler = DefaultMouseEventHandler.optional()
 
@@ -70,7 +132,34 @@ export const MouseEventHandlers = z.object({
     onMouseUpCapture: OptionalDefaultMouseEventHandler,
 })
 
-const DefaultFormEventHandler = z.function(z.tuple([]), z.undefined())
+export const CommonKeyEvent = BaseEvent.augment({
+    altKey: z.boolean(),
+    charCode: z.number(),
+    ctrlKey: z.boolean(),
+    key: z.string(),
+    keyCode: z.number(),
+    locale: z.string(),
+    location: z.number(),
+    metaKey: z.boolean(),
+    repeat: z.boolean(),
+    shiftKey: z.boolean(),
+    which: z.number(),
+})
+
+const DefaultKeyEventHandler = zEventHandler(CommonKeyEvent)
+
+const OptionalDefaultKeyEventHandler = DefaultKeyEventHandler.optional()
+
+export const KeyEventHandlers = z.object({
+    onKeyDown: OptionalDefaultKeyEventHandler,
+    onKeyDownCapture: OptionalDefaultKeyEventHandler,
+    onKeyPress: OptionalDefaultKeyEventHandler,
+    onKeyPressCapture: OptionalDefaultKeyEventHandler,
+    onKeyUp: OptionalDefaultKeyEventHandler,
+    onKeyUpCapture: OptionalDefaultKeyEventHandler,
+})
+
+const DefaultFormEventHandler = zEventHandler(BaseEvent)
 const OptionalDefaultFormEventHandler = DefaultFormEventHandler.optional()
 export const FormEventHandlers = z.object({
     onChange: OptionalDefaultFormEventHandler,
@@ -93,3 +182,4 @@ export const Common = Initial.merge(Fill)
     .merge(Shadow)
     .merge(Layout)
     .merge(MouseEventHandlers)
+    .merge(KeyEventHandlers)
