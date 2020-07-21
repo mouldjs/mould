b733cf96cad34b4b9c17b2f9d96ed3e50b2ad984
b1ncer <b1ncer@foxmail.com>
2020-07-21 16:36:18 +0800

    fix(Runtime): adjust hooks return value (#142)
    * no message
    
    * fix type issue

--- app/types.ts
+++ app/types.ts
@@ -101,7 +101,7 @@ export type AtomicComponent = {
     acceptChildren?: boolean
 }
 
-export type useScopeFn = (input: object) => [StateName, object]
+export type useScopeFn = (input: object) => [object, StateName]
 
 export type InspectorProps<T, Option = {}> = {
     data: T | undefined
