0047ed75ab6c229df742560ad8ae4df625276882
lightrabbit <lightpacerabbit@gmail.com>
2020-07-24 14:35:42 +0800

    feat(compiler): let the compiler support the parent context

--- mould.ts
+++ mould.ts
@@ -8,6 +8,10 @@ type TransformsByType = {
     [key: string]: AtomicComponent['Transform']
 }
 
+type AtomicComponentByType = {
+    [key: string]: AtomicComponent | undefined
+}
+
 export const components: ComponentsByType = Components.reduce(
     (componentsByType, { type, Raw }) => ({
         ...componentsByType,
@@ -21,4 +25,11 @@ export const transforms: TransformsByType = Components.reduce(
         [type]: Transform,
     }),
     {} as TransformsByType
+)
+export const definitions: AtomicComponentByType = Components.reduce(
+    (atomicComponentsByType, definition) => ({
+        ...atomicComponentsByType,
+        [definition.type]: definition,
+    }),
+    {} as AtomicComponentByType
 )
