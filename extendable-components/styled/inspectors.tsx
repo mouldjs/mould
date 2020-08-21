1fc4924e153fbd9f8b9d435660479730a32558d1
lightrabbit <lightpacerabbit@gmail.com>
2020-08-21 21:05:55 +0800

    feat(inspector): add utility for extended component (#176)

---
+++ extendable-components/styled/inspectors.tsx
@@ -0,0 +1,47 @@
+import { LayoutInspector } from '../../inspector/Layout'
+import * as z from 'zod'
+import { BaseComponentProps } from './baseprops'
+import { SFC } from 'react'
+import { FillInspector } from '../../inspector/Fill'
+import { ParentContext } from '../../app/types'
+import { ContainerRelatedInspectors } from '../../inspector/InspectorProvider'
+import { BorderInspector } from '../../inspector/Border'
+var y = z.enum(['a', 'b'])
+var x: z.infer<typeof y>
+
+export const BaseComponentInspectors: SFC<{
+    parent: ParentContext | undefined
+    data: BaseComponentProps
+    requestUpdateProps: (newValue: BaseComponentProps) => void
+}> = ({ data, requestUpdateProps, parent }) => {
+    return (
+        <>
+            <ContainerRelatedInspectors
+                parent={parent}
+                data={data.containerLayoutProps}
+                onChange={(data) => {
+                    requestUpdateProps({
+                        containerLayoutProps: data,
+                    })
+                }}
+            ></ContainerRelatedInspectors>
+            <LayoutInspector
+                title="Layout"
+                data={data.layoutProps}
+                onChange={(layoutProps) => requestUpdateProps({ layoutProps })}
+            ></LayoutInspector>
+            <FillInspector
+                title="Fill"
+                data={data.fillProps}
+                onChange={(fillProps) => requestUpdateProps({ fillProps })}
+            ></FillInspector>
+            <BorderInspector
+                title="Border"
+                data={data.borderProps}
+                onChange={(borderProps) => {
+                    requestUpdateProps({ borderProps })
+                }}
+            ></BorderInspector>
+        </>
+    )
+}
